6bc557995bbc0671e9d69b23f9cb27b5c2af11cd
Mirek Simek <miroslav.simek@gmail.com>
2022-02-06 09:21:03 +0100

    Import bugfix

--- oarepo_model_builder_multilingual/model_preprocessors/multilingual.py
+++ oarepo_model_builder_multilingual/model_preprocessors/multilingual.py
@@ -3,4 +3,4 @@ from oarepo_model_builder.model_preprocessors import ModelPreprocessor
 class MultilingualModelPreprocessor(ModelPreprocessor):
     def transform(self, schema, settings):
         self.set(settings.python, 'multilingual-dumper-class',
-                 lambda: f'{settings.package}.services.multilingual_dumper.MultilingualDumper')
+                 lambda: f'{settings.python.record_records_package}.multilingual_dumper.MultilingualDumper')
