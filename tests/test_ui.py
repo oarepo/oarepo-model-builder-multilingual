ba1887849464547a53f8d105a523eac8f9bcb7f1
Alzpetka <lili3.pokorna@seznam.cz>
2022-08-29 15:04:48 +0200

    test ui

---
+++ tests/test_ui.py
@@ -0,0 +1,28 @@
+from oarepo_model_builder.entrypoints import create_builder_from_entrypoints, load_model
+
+from tests.mock_filesystem import MockFilesystem
+
+
+def test_validity():
+    schema = load_model(
+        "test.yaml",
+        "test",
+        model_content={"oarepo:use": "invenio", "settings": {"supported-langs": {
+            'en': {
+                'text': {
+                    'analyzer': 'czech'
+                },
+                'sort': {
+                    'type': 'icu_collation_keyword'
+                }
+            }
+        }},
+                       "model": {"properties": {"a": {"type": "multilingual", "oarepo:ui": {} }}}},
+        isort=False,
+        black=False,
+    )
+
+    filesystem = MockFilesystem()
+    builder = create_builder_from_entrypoints(filesystem=filesystem)
+
+    builder.build(schema, "")
