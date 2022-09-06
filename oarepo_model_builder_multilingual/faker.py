48f3c6afb2a1d9aca0654d5b810bc1fcdcc0f2ac
Alzpetka <lili3.pokorna@seznam.cz>
2022-09-06 14:44:37 +0200

    i18nStr sample generator

--- oarepo_model_builder_multilingual/faker.py
+++ oarepo_model_builder_multilingual/faker.py
@@ -5,21 +5,46 @@ from oarepo_model_builder.invenio.invenio_script_sample_data import SKIP
 def multilingual_sample_provider(faker, settings, stack, params):
     if stack.top.schema_element_type not in ('property', 'items'):
         return SKIP
-    if stack.top.data.get('type') != 'multilingual':
+
+    if stack.top.data.get('type') == 'multilingual':
+        def get_faker(lang):
+            try:
+                return Faker(lang)
+            except:
+                return Faker()
+
+        return [
+            {
+                'lang': lang,
+                'value': get_faker(lang).sentence(nb_words=6)
+            }
+            for lang in faker.random_elements(elements=settings['supported_langs'].keys(),
+                                              length=min(3, len(settings['supported_langs'].keys())),
+                                              unique=True)
+        ]
+
+    elif stack.top.data.get('type') == 'i18nStr':
+        def get_faker(lang):
+            try:
+                return Faker(lang)
+            except:
+                return Faker()
+
+        definition = stack.top.data.get('oarepo:multilingual', {})
+        lang_name = definition.get('lang-field', 'lang')
+        field_name = definition.get('value-field', 'value')
+        return [
+            {
+                lang_name: lang,
+                field_name: get_faker(lang).sentence(nb_words=6)
+            }
+            for lang in faker.random_elements(elements=settings['supported_langs'].keys(),
+                                              length=min(3, len(settings['supported_langs'].keys())),
+                                              unique=True)
+        ]
+    else:
         return SKIP
 
-    def get_faker(lang):
-        try:
-            return Faker(lang)
-        except:
-            return Faker()
 
-    return [
-        {
-            'lang': lang,
-            'value': get_faker(lang).sentence(nb_words=6)
-        }
-        for lang in faker.random_elements(elements=settings['supported_langs'].keys(),
-                                          length=min(3, len(settings['supported_langs'].keys())),
-                                          unique=True)
-    ]
+
+
