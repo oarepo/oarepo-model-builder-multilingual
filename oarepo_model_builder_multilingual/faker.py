27ca25fa9816c3125982427419ed8f8035ff98e2
Alzpetka <lili3.pokorna@seznam.cz>
2022-09-06 15:00:22 +0200

    i18nStr sample generator

--- oarepo_model_builder_multilingual/faker.py
+++ oarepo_model_builder_multilingual/faker.py
@@ -1,6 +1,6 @@
 from faker import Faker
 from oarepo_model_builder.invenio.invenio_script_sample_data import SKIP
-
+import random
 
 def multilingual_sample_provider(faker, settings, stack, params):
     if stack.top.schema_element_type not in ('property', 'items'):
@@ -33,15 +33,12 @@ def multilingual_sample_provider(faker, settings, stack, params):
         definition = stack.top.data.get('oarepo:multilingual', {})
         lang_name = definition.get('lang-field', 'lang')
         field_name = definition.get('value-field', 'value')
-        return [
-            {
+        lang = random.choice(list(settings['supported_langs'].keys()))
+        return {
                 lang_name: lang,
                 field_name: get_faker(lang).sentence(nb_words=6)
             }
-            for lang in faker.random_elements(elements=settings['supported_langs'].keys(),
-                                              length=min(3, len(settings['supported_langs'].keys())),
-                                              unique=True)
-        ]
+
     else:
         return SKIP
 
