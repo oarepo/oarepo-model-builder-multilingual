2b4d6ba8ad22dea7620c8d912035c5e18244973c
Alzpetka <lili3.pokorna@seznam.cz>
2023-03-27 13:51:28 +0200

    dependencies

--- run-tests.sh
+++ run-tests.sh
@@ -5,6 +5,7 @@ set -e
 python3 -m venv .venv
 .venv/bin/pip install -U setuptools pip wheel
 .venv/bin/pip install -e .
+.venv/bin/pip install -e .['tests']
 .venv/bin/pip install oarepo-model-builder
 
 
