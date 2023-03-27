823273712cc08aafbf2619d090a77c89b6cb1408
Alzpetka <lili3.pokorna@seznam.cz>
2023-03-27 13:55:51 +0200

    tests

--- run-tests.sh
+++ run-tests.sh
@@ -5,7 +5,6 @@ set -e
 python3 -m venv .venv
 .venv/bin/pip install -U setuptools pip wheel
 .venv/bin/pip install -e .
-.venv/bin/pip install -e .['tests']
 .venv/bin/pip install oarepo-model-builder
 
 
