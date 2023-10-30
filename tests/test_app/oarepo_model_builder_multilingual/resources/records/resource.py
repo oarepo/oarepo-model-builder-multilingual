c29f70c34e7a8e9c40bd2c68fabd1feead58dd98
Alzpetka <lili3.pokorna@seznam.cz>
2023-10-30 13:06:40 +0100

    dumper imports

--- tests/test_app/oarepo_model_builder_multilingual/resources/records/resource.py
+++
@@ -1,8 +0,0 @@
-from invenio_records_resources.resources import RecordResource
-
-
-class OarepoModelBuilderMultilingualResource(RecordResource):
-    """OarepoModelBuilderMultilingualRecord resource."""
-
-    # here you can for example redefine
-    # create_url_rules function to add your own rules
