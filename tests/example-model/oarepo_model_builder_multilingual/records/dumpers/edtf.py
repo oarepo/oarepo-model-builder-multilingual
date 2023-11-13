87adf84695f2c8795858757a5558d32c0ce02d3b
Alzpetka <lili3.pokorna@seznam.cz>
2023-11-13 07:56:19 +0100

    tests

--- tests/example-model/oarepo_model_builder_multilingual/records/dumpers/edtf.py
+++
@@ -1,7 +0,0 @@
-from oarepo_runtime.records.dumpers.edtf_interval import EDTFIntervalDumperExt
-
-
-class OarepoModelBuilderMultilingualEDTFIntervalDumperExt(EDTFIntervalDumperExt):
-    """edtf interval dumper."""
-
-    paths = []
--- tests/example-model/oarepo_model_builder_multilingual/services/records/service.py
+++
@@ -1,5 +0,0 @@
-from invenio_records_resources.services import RecordService as InvenioRecordService
-
-
-class OarepoModelBuilderMultilingualService(InvenioRecordService):
-    """OarepoModelBuilderMultilingualRecord service."""
