99bb54d61bd467abc76bea6cdff7b65c0e717bf5
Alzbeta Pokorna <alzbetapokorna@Alzbetas-MacBook-Pro.local>
2025-02-04 09:24:43 +0100

    rdm dependency

--- run-tests.sh
+++ run-tests.sh
@@ -8,7 +8,8 @@ BUILDER_VENV=".venv-builder"
 if test -d $BUILDER_VENV ; then
 	rm -rf $BUILDER_VENV
 fi
-
+export PIP_EXTRA_INDEX_URL=https://gitlab.cesnet.cz/api/v4/projects/1408/packages/pypi/simple
+export UV_EXTRA_INDEX_URL=https://gitlab.cesnet.cz/api/v4/projects/1408/packages/pypi/simple
 python3 -m venv $BUILDER_VENV
 . $BUILDER_VENV/bin/activate
 pip install -U setuptools pip wheel oarepo-model-builder-ui json5
@@ -35,6 +36,6 @@ python3 -m venv $VENV_TESTS
 source $VENV_TESTS/bin/activate
 
 pip install -U setuptools pip wheel
-pip install "oarepo[tests]==${OAREPO_VERSION}.*"
+pip install "oarepo[tests, rdm]==${OAREPO_VERSION}.*"
 pip install "./example-model[tests]"
 pytest ./example-model/tests -vvv
